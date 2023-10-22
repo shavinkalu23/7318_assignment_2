14a792fc2145bde181db7b1a2f823994ab4c3795
Shavin Kaluthantri <shavinkaluthanthri@gmail.com>
2023-10-22 15:33:51 +1030

    created base model

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import torch
-from torchvision import datasets, transforms
+import torch 
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 transform = transforms.Compose([
     transforms.Resize((32, 32)),
@@ -7,8 +12,85 @@ transform = transforms.Compose([
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
-train_data = datasets.ImageFolder(root='GTSRB/Final_Training/Images', transform=transform)
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
 
-test_data = datasets.ImageFolder(root='GTSRB/Final_Test/Images', transform=transform)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=False)
+trainset = torchvision.datasets.GTSRB(root='./data', split = 'train',
+                                        download=True, transform=transform)
+
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.GTSRB(root='./data', split = 'test',
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64,
+                                         shuffle=False, num_workers=2)
+
+
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    plt.imshow(img.permute(1, 2, 0))
+    plt.show()
+
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+imshow(torchvision.utils.make_grid(images[:4]))
+print('Labels:', labels[:4])
+
+class BaseCNN(nn.Module):
+    def __init__(self):
+        super(BaseCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, 5)
+        self.fc1 = nn.Linear(32 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 43)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 32 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+    
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = BaseCNN().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+# Training
+for epoch in range(10):
+    running_loss = 0.0
+    for i, (inputs, labels) in enumerate(trainloader, 0):
+        inputs, labels = inputs.to(device), labels.to(device)
+        
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+    print(f"Epoch {epoch+1}, Loss: {running_loss/len(trainloader)}")
+
+print("Finished Training")
+
+# Evaluation
+correct = 0
+total = 0
+with torch.no_grad():
+    for (inputs, labels) in testloader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f"Accuracy of the base model on test images: {100 * correct / total}%")
+
