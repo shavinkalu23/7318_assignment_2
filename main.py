14f07d1fbfb41db6285e43e0398a88adba6f1c96
Shavin Kaluthantri <shavinkaluthanthri@gmail.com>
2023-10-22 13:18:10 +1030

    changed to GTSRB Datset to reduce size of dataset

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import torch
-import torchvision
-import torchvision.transforms as transforms
+from torchvision import datasets, transforms
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform = transforms.Compose([
+    transforms.Resize((32, 32)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
 
-trainset = torchvision.datasets.Food101(root='./data', split="train",
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
+train_data = datasets.ImageFolder(root='GTSRB/Final_Training/Images', transform=transform)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
 
-testset = torchvision.datasets.Food101(root='./data', split = "test",
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
+test_data = datasets.ImageFolder(root='GTSRB/Final_Test/Images', transform=transform)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=False)
