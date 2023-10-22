d157f5e128a2a49791c276df2ff9b981550212ec
Shavin Kaluthantri <shavinkaluthanthri@gmail.com>
2023-10-22 17:00:23 +1030

    added vgg16 and resnet models

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torchvision import models
 
 transform = transforms.Compose([
     transforms.Resize((32, 32)),
@@ -17,12 +18,12 @@ trainset = torchvision.datasets.GTSRB(root='./data', split = 'train',
                                         download=True, transform=transform)
 
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=64,
                                           shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.GTSRB(root='./data', split = 'test',
                                        download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=64,
+test_loader = torch.utils.data.DataLoader(testset, batch_size=64,
                                          shuffle=False, num_workers=2)
 
 
@@ -31,8 +32,8 @@ def imshow(img):
     plt.imshow(img.permute(1, 2, 0))
     plt.show()
 
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
+dataiter = iter(train_loader)
+images, labels = next(iter(train_loader))
 imshow(torchvision.utils.make_grid(images[:4]))
 print('Labels:', labels[:4])
 
@@ -67,7 +68,7 @@ optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 # Training
 for epoch in range(10):
     running_loss = 0.0
-    for i, (inputs, labels) in enumerate(trainloader, 0):
+    for i, (inputs, labels) in enumerate(train_loader, 0):
         inputs, labels = inputs.to(device), labels.to(device)
         
         optimizer.zero_grad()
@@ -77,7 +78,7 @@ for epoch in range(10):
         optimizer.step()
         
         running_loss += loss.item()
-    print(f"Epoch {epoch+1}, Loss: {running_loss/len(trainloader)}")
+    print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}")
 
 print("Finished Training")
 
@@ -85,7 +86,7 @@ print("Finished Training")
 correct = 0
 total = 0
 with torch.no_grad():
-    for (inputs, labels) in testloader:
+    for (inputs, labels) in test_loader:
         inputs, labels = inputs.to(device), labels.to(device)
         outputs = model(inputs)
         _, predicted = outputs.max(1)
@@ -94,3 +95,127 @@ with torch.no_grad():
 
 print(f"Accuracy of the base model on test images: {100 * correct / total}%")
 
+#### VGG 16 MODEL
+
+
+# Load the VGG16 model
+vgg16 = models.vgg16(pretrained=False)  # We use a non-pretrained model for this task
+vgg16.classifier[6] = nn.Linear(4096, 43)  # Change the last layer to have 43 outputs
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = vgg16.to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+# Training
+num_epochs = 10
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for i, (inputs, labels) in enumerate(train_loader, 0):
+        inputs, labels = inputs.to(device), labels.to(device)
+        
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+    print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}")
+
+print("Finished Training")
+
+# Evaluation
+correct = 0
+total = 0
+with torch.no_grad():
+    for (inputs, labels) in test_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f"Accuracy of the VGG16 model on test images: {100 * correct / total}%")
+
+
+
+#### implementing resnet
+
+
+# Load the ResNet18 model
+resnet18 = models.resnet18(pretrained=False)  # We use a non-pretrained model for this task
+resnet18.fc = nn.Linear(resnet18.fc.in_features, 43)  # Change the final fully connected layer to have 43 outputs
+
+model = resnet18.to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+# Training
+num_epochs = 10
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for i, (inputs, labels) in enumerate(train_loader, 0):
+        inputs, labels = inputs.to(device), labels.to(device)
+        
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+    print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}")
+
+print("Finished Training")
+
+# Evaluation
+correct = 0
+total = 0
+with torch.no_grad():
+    for (inputs, labels) in test_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f"Accuracy of the ResNet18 model on test images: {100 * correct / total}%")
+
+
+
+model = resnet18.to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+# Training
+num_epochs = 10
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for i, (inputs, labels) in enumerate(train_loader, 0):
+        inputs, labels = inputs.to(device), labels.to(device)
+        
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+    print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}")
+
+print("Finished Training")
+
+# Evaluation
+correct = 0
+total = 0
+with torch.no_grad():
+    for (inputs, labels) in test_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f"Accuracy of the ResNet18 model on test images: {100 * correct / total}%")
